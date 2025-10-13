9c321197f6a29eba7e887ff49596cdb6d3b1de19
Pratik Thapw <pratikthapw@gmail.com>
2025-10-13 14:22:53 +0545

    chore(monorepo): initial setup

---
+++ apps/portal/.next/server/middleware-build-manifest.js
@@ -0,0 +1,55 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": [
+      "static/chunks/c6b16_next_dist_compiled_next-devtools_index_afaed119.js",
+      "static/chunks/c6b16_next_dist_compiled_c7dbd689._.js",
+      "static/chunks/c6b16_next_dist_shared_lib_e9976687._.js",
+      "static/chunks/c6b16_next_dist_client_15a6718b._.js",
+      "static/chunks/c6b16_next_dist_5c0e359e._.js",
+      "static/chunks/c6b16_next_app_b9f4edf2.js",
+      "static/chunks/[next]_entry_page-loader_ts_6fefa396._.js",
+      "static/chunks/c046a_react-dom_812c9f31._.js",
+      "static/chunks/node_modules__pnpm_f012a4b7._.js",
+      "static/chunks/[root-of-the-server]__247cbeb5._.js",
+      "static/chunks/apps_portal_pages__app_2da965e7._.js",
+      "static/chunks/turbopack-apps_portal_pages__app_5982016c._.js"
+    ],
+    "/_error": [
+      "static/chunks/c6b16_next_dist_compiled_next-devtools_index_afaed119.js",
+      "static/chunks/c6b16_next_dist_compiled_c7dbd689._.js",
+      "static/chunks/c6b16_next_dist_shared_lib_b7d46416._.js",
+      "static/chunks/c6b16_next_dist_client_15a6718b._.js",
+      "static/chunks/c6b16_next_dist_5ca0d194._.js",
+      "static/chunks/c6b16_next_error_bc5d4766.js",
+      "static/chunks/[next]_entry_page-loader_ts_27a4e137._.js",
+      "static/chunks/c046a_react-dom_812c9f31._.js",
+      "static/chunks/node_modules__pnpm_f012a4b7._.js",
+      "static/chunks/[root-of-the-server]__8f3ec8f1._.js",
+      "static/chunks/apps_portal_pages__error_2da965e7._.js",
+      "static/chunks/turbopack-apps_portal_pages__error_4dcc0d48._.js"
+    ]
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/c6b16_next_dist_build_polyfills_polyfill-nomodule.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_97c46533._.js",
+    "static/chunks/c6b16_next_dist_compiled_react-dom_ffcf0ef6._.js",
+    "static/chunks/c6b16_next_dist_compiled_next-devtools_index_ad1936a3.js",
+    "static/chunks/c6b16_next_dist_compiled_730bcc36._.js",
+    "static/chunks/c6b16_next_dist_client_cb5c7112._.js",
+    "static/chunks/c6b16_next_dist_d09e5c9c._.js",
+    "static/chunks/69652_@swc_helpers_cjs_77b72907._.js",
+    "static/chunks/apps_portal_a0ff3932._.js",
+    "static/chunks/turbopack-apps_portal_031890a0._.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
