373f5849281af3621fd0190426f96f15f7a948a0
Pratik Thapw <pratikthapw@gmail.com>
2025-10-15 09:09:15 +0545

    refactore(monorepo): css transpile working on portal apps also

--- apps/portal/app/page.tsx
+++ apps/portal/app/page.tsx
@@ -1,12 +1,11 @@
-import ComingSoon from '@/components/coming-soon'
 import ConnectWallet from '@/components/connect-wallet'
 import React from 'react'
 
 const Page = () => {
   return (
-    <>
-      <ComingSoon />
-    </>
+    <div className='min-h-full mx-auto flex justify-center items-center'>
+      <ConnectWallet />
+    </div>
   )
 }
 
--- apps/portal/next.config.mjs
+++ apps/portal/next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-        port: "",
-        pathname: "/**",
-      },
-    ]
-  }
 }
 
 export default nextConfig
